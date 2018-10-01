612378b3634d9a55054e6cae3828182113534dcf
Juanmamd <jm.munoz.dom@gmail.com>
2018-10-02 00:21:52 +0200

    Schedules finished
    All bugs solved.

--- control-placas.v-0.4/control-placas.v-0.4.ino
+++ control-placas.v-0.4/control-placas.v-0.4.ino
@@ -40,24 +40,21 @@
 #include <max6675.h>
 
 #define I2C_ADDR    0x3F    // 0x27 in PCF8574 by NXP and Set to 0x3F in PCF8574A
-// #define LCD_DISPLAY true
-// #define RF24 true
 
 // #ifdef RF24
   #include <SPI.h>
   #include "RF24.h"
   #include <LCD.h>
   #include <LiquidCrystal_I2C.h>
-
   RF24 radio(9,10);
 // #endif
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+// Thermpair
 // Pin donde se conecta el bus 1-Wire
 const int pinDatosDQ = 7;
-
 // Instancia a las clases OneWire y DallasTemperature
 OneWire oneWireObjeto(pinDatosDQ);
 DallasTemperature sensorDS18B20(&oneWireObjeto);
@@ -76,17 +73,19 @@ int ProgrammedLed=4;
 //System variables
 int debug=0;
 int loop_time = 500;  					      // time to sleep earch loop, in secs
-int time_per_degree = 5; 			    // time to raise 1 degree, in secs, initial value
+int time_per_degree = 60; 			    // time to raise 1 degree, in secs, initial value
 int heater_autoadjust = 0;				      // weather time_per_degree is auto adjust from previous data
 int max_running_time = 4 * 60;			// 4 mins maximum running time, default = 4 hours
 
-int eeprom_pos=100;
+int eeprom_pos=100;     // pos 98    1=saved   0=not saved
+                        // pos 99    num_schedules
+                        // pos 100   begin saved schedules
 struct dailyprog {
   byte weekday;  // indexed by week[]
   byte use_time;  // time from 4 am to 23 am, 5 minutes interval.
 } ;
 dailyprog schedule[14]; // two schedules per day.
-char week[]={"DLMXJVS"};
+char week[]={" DLMXJVS"}; // Week begins in 1, sunday
 int num_schedules;
 
 //Internal variables
@@ -115,21 +114,21 @@ int perform_adjust=0;
 // #endif
 
 void setup() {
-  schedule[0].weekday=0;
+  schedule[0].weekday=1;
   schedule[0].use_time=75;  // 10:15 am.  (10-4)*12 + 15/5=75  75*5=375  375/60 = 6,25 6h 15min + 4.00 = 10,15
   schedule[1].weekday=1;
   schedule[1].use_time=38;   // 7:00    7-4=3 3*12=36
   schedule[2].weekday=2;
-  schedule[2].use_time=39;   // 7:00    7-4=3 3*12=36
+  schedule[2].use_time=39;   // 7:05    7-4=3 3*12=36
   schedule[3].weekday=3;
-  schedule[3].use_time=40;   // 7:00    7-4=3 3*12=36
+  schedule[3].use_time=40;   // 7:10    7-4=3 3*12=36
   schedule[4].weekday=4;
-  schedule[4].use_time=41;   // 7:00    7-4=3 3*12=36
+  schedule[4].use_time=41;   // 7:20    7-4=3 3*12=36
   schedule[5].weekday=5;
-  schedule[5].use_time=42;   // 7:00    7-4=3 3*12=36
+  schedule[5].use_time=42;   // 7:25    7-4=3 3*12=36
   schedule[6].weekday=6;
-  schedule[6].use_time=43;   // 7:00    7-4=3 3*12=36
-  schedule[7].weekday=1;
+  schedule[6].use_time=43;   // 7:30    7-4=3 3*12=36
+  schedule[7].weekday=7;
   schedule[7].use_time=198;  // 20:30 am.  (20-4)*12=192 + 30/5=6 198 75*5=375  375/60 = 6,25 6h 15min + 4.00 = 10,15
   num_schedules=8;
 
@@ -141,37 +140,24 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(stop_button),stop_programm_int, RISING); // Stop programm interrupt
   attachInterrupt(digitalPinToInterrupt(displayLine),display_show_line,RISING);
   Serial.begin(115200);
-  // #ifdef RF24
-        radio.begin();
-        radio.setPALevel(RF24_PA_LOW);
-        radio.openWritingPipe(pipe);
-        radio.openReadingPipe(1,pipe);
-    // #endif
-  ///
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.openWritingPipe(pipe);
+  radio.openReadingPipe(1,pipe);
   sensorDS18B20.begin();
   date = get_date(); // Ask for remote date
   set_date(date);
-  // Prototype. Schedule from 10 seconds to 110 seconds from now.
-
-  // next_schedule_ini=now()+10;
-  // next_schedule_fin=next_schedule_ini+100;
   show_schedules(schedule);
   load_schedules(schedule);
-  // next_schedule = get_next_schedule(schedule);				// Get next time when water has to be warmed, in secs
 
   digitalWrite(ProgrammedLed,!stop_programm);
-  // sensors.begin();
-// #ifdef LCD_DISPLAY
-    lcd.begin(16,2,LCD_5x8DOTS);
-    lcd.createChar(0, char_OFF);
-    lcd.createChar(1, char_ON);
-    lcd.clear();
-    // lcd.command(LCD_NOBACKLIGHT );
-    // lcd.noBackLight();
-    lcd.setCursor(0,0);
-    lcd.print("Water heater IoT");
-    delay(1000);
-// #endif
+  lcd.begin(16,2,LCD_5x8DOTS);
+  lcd.createChar(0, char_OFF);
+  lcd.createChar(1, char_ON);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Water heater IoT");
+  delay(1000);
 };
 
 void loop(){
@@ -181,10 +167,13 @@ void loop(){
   water_temp = get_watertemp();
   time_to_warm = ( target_temp - water_temp) * time_per_degree;		// current time to warm water at this temperature, in secs
   next_schedule = get_next_schedule(schedule);				// Get next time when water has to be warmed, in secs
+  Serial.println("Next:");
   print_time(next_schedule);
+  Serial.println("Now:");
+  print_time(t_now);
   time_to_sched=next_schedule - t_now;
   Serial.print("Time to sched: ");
-  Serial.println(time_to_sched);
+  Serial.println(time_to_minutes(time_to_sched));
   if (heater_on) running_time = t_now - init_time;		// Calculate running time for displaying
   if (water_temp < target_temp && time_to_warm > time_to_sched && t_now < next_schedule && !stop_programm) {
     //if(debug)Serial.println("Calentando");
@@ -206,14 +195,21 @@ void loop(){
   display(heater_on, water_temp, target_temp, running_time, now(), next_schedule);
   // if( (count % 5) ==0 ) send_data();
   delay(loop_time);
+  if(count % 5 == 0)display_show_line();
   count++;
   //if(debug) Serial.println(count);
 };
 
+int time_to_minutes(time_t inputtime){
+  int return_minutes;
+  return_minutes=day(inputtime)*1440 + hour(inputtime)*60 + minute(inputtime);
+  return return_minutes;
+};
+
 void display_show_line(){
   display_line_i++;
-  Serial.print("Displaying line ");
-  Serial.println(display_line_i);
+  // Serial.print("Displaying line ");
+  // Serial.println(display_line_i);
   if(display_line_i>NUM_DISPLAYS)display_line_i=1;
   delay(50);
 };
@@ -285,7 +281,7 @@ void stop_programm_int(){
   };
 
 int set_date(time_t date){  // Need to implement an NTP-like using RF24 get_date()
-  setTime(23,00,00,30,9,2018);
+  setTime(23,50,00,1,10 ,2018);
   };
 
 time_t get_date(){ // TODO
@@ -313,9 +309,9 @@ float get_watertemp(){  //Implement according to hardware sensor
     float sensortemp;
     sensorDS18B20.requestTemperatures();
     sensortemp=sensorDS18B20.getTempCByIndex(0);
-    Serial.println("");
+    Serial.print("Temperature: ");
     Serial.println(sensortemp);
-    Serial.println("");
+    // Serial.println("");
     return sensortemp;
 };
 
@@ -324,48 +320,47 @@ time_t get_next_schedule(struct dailyprog *sched){ // TODO
   tmElements_t timeElements;
   int target_weekday=weekday(t_now);
   int sch_i=0;
-  int time_found=1440; // One day minutes
+  int time_found=14400; // Ten days in minutes
   int time1, time2, sch_found;
   boolean next_found=false;
 
-  time2=hour(t_now)+minute(t_now);
-  while(!next_found){
-    while(sch_i < num_schedules){
-      Serial.print("Searching next schedule: ");
-      Serial.println(sch_i);
-      if(sched[sch_i].weekday==target_weekday){
-        time1=(sched[sch_i].use_time/12 + 4)*60 + sched[sch_i].use_time%12 * 5;
-        Serial.print("Weekday found: ");
-        Serial.print(time1);
-        Serial.print(" / ");
-        Serial.println(time2);
-        if(time1-time2<time_found){
-          time_found=time1-time2;
-          sch_found=sch_i;
-          Serial.print("Sched found: ");
-          Serial.println(sch_i);
-        };
+  time1=weekday(t_now)*1440 + hour(t_now)*60 + minute(t_now);  // Now in minutes since past sunday
+  while(sch_i < num_schedules){
+    // Serial.print("Searching next schedule: ");
+    // Serial.println(sch_i);
+      time2=sched[sch_i].weekday*1440 + (sched[sch_i].use_time/12 + 4)*60 + sched[sch_i].use_time%12 * 5;
+      // Serial.print("Weekday found: ");
+      // Serial.print(time2);
+      // Serial.print(" / ");
+      // Serial.print(time1);
+      // Serial.print(" / ");
+      // Serial.println(time2-time1);
+      if(time2-time1<time_found && time2-time1>0){  // search for minimum difference (next schedule)
+        time_found=time2-time1;
+        sch_found=sch_i;
+        // Serial.print("Sched found: ");
+        // Serial.println(sch_i);
       };
-      sch_i++;
-    };
-    target_weekday++;
-    if(target_weekday>6)target_weekday=0;
+    sch_i++;
   };
   timeElements={0,sched[sch_found].use_time%12 * 5, sched[sch_found].use_time/12 + 4,
-                  weekday(t_now),day(t_now),month(t_now),year(t_now)-1970};
+                  sched[sch_found].weekday,day(t_now)+(sched[sch_found].weekday-weekday(t_now)),
+                  month(t_now),year(t_now)-1970};
   return_time=makeTime(timeElements);
-  Serial.println("Next schedule");
-  print_time(return_time);
+  // Serial.println("Next schedule");
+  // print_time(return_time);
   return return_time;
   };
 
 void print_time(time_t sched){
-  Serial.print(day(sched));
-  Serial.print("/");
-  Serial.print(month(sched));
-  Serial.print("/");
-  Serial.print(year(sched));
-  Serial.print(" ");
+  if(year(sched)>1970){
+    Serial.print(day(sched));
+    Serial.print("/");
+    Serial.print(month(sched));
+    Serial.print("/");
+    Serial.print(year(sched));
+    Serial.print(" ");
+  };
   Serial.print(hour(sched));
   Serial.print(":");
   Serial.print(minute(sched));
@@ -388,8 +383,7 @@ int power_off_heater(){ // TODO Implement relay control
 int log_session(){}; // TODO
 
 int display(int h_status, float temp1, float temp2, int r_time, time_t today, time_t next){
-  char buff1[5];
-  char buff2[5];
+  char buff1[5], buff2[5];
   // char *buff3="";
   // TODO implement formatting LCD output
   // sprintf(buff3,"Param: %2.2f / %2.2f", temp1, temp2);
@@ -401,7 +395,8 @@ int display(int h_status, float temp1, float temp2, int r_time, time_t today, ti
   sprintf(display_line [0], "%02d/%02d    %02d:%02d  ",day(today),month(today),hour(today),minute(today));
   sprintf(display_line [1], "T: %s->%s", buff1, buff2);
   // sprintf(display_line [1], "Temp: %02dC->%02dC  ", temp1, temp2);
-  sprintf(display_line [2], "Linea 2         ");
+  sprintf(display_line [2], "%02d/%02d    %02d:%02d  ",day(next),month(next),hour(next),minute(next));
+  // sprintf(display_line [2], "Linea 2         ");
   sprintf(display_line [3], "Linea 3         ");
   if(display_active){ // Display Active
     lcd.setCursor(0,0);
