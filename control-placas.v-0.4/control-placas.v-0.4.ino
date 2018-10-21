7df29fb85cf3f54b1562c5fb7d87f0dc05ac2fbb
Juanmamd <jm.munoz.dom@gmail.com>
2018-10-21 23:51:55 +0200

    EEPROM ok, status chars

--- control-placas.v-0.4/control-placas.v-0.4.ino
+++ control-placas.v-0.4/control-placas.v-0.4.ino
@@ -44,10 +44,11 @@
 #include <Time.h>
 #include <TimeLib.h>
 #include <Wire.h>
-#include "RTClib.h"
+#include <RTClib.h>
 
 // #include <max6675.h>
 #include <avr/pgmspace.h>
+#include <EEPROM.h>
 
 
 #include <SPI.h>
@@ -62,7 +63,7 @@ RF24 radio(9,10);
 #include <DallasTemperature.h>    // For DS18B20
 // Thermpair
 // Pin donde se conecta el bus 1-Wire
-const int pinDatosDQ = 7; // for DS18B20
+const byte pinDatosDQ = 7; // for DS18B20
 // Instancia a las clases OneWire y DallasTemperature
 OneWire oneWireObjeto(pinDatosDQ);
 DallasTemperature sensorDS18B20(&oneWireObjeto);
@@ -73,12 +74,12 @@ const float hister_temp PROGMEM =2;   //histeresis
 // float target_temp = 35;
 
 // hardware variables
-const int sensorPin PROGMEM  =  A0;     // select the input  pin for the potentiometer
-const int HeaterPin PROGMEM  =  6;      // select the pin for  the LED, relay emulation
+const byte sensorPin PROGMEM  =  A0;     // select the input  pin for the potentiometer
+const byte HeaterPin PROGMEM  =  6;      // select the pin for  the LED, relay emulation
 const byte stop_button PROGMEM = 2;     // Stop programming / manual mode
 const byte displayPin PROGMEM=5;        // Display config
 const byte displayLine PROGMEM=3;       // scroll display lines
-const int ProgrammedLed PROGMEM=4;
+const byte ProgrammedLed PROGMEM=4;
 
 //System variables
 int debug=1;
@@ -86,11 +87,11 @@ int debug=1;
 
 int loop_time = 100;  					     // time to sleep earch loop, in secs
 int time_per_degree = 300; 			     // time to raise 1 degree, in secs, initial value
-int heater_autoadjust = 1;				   // weather time_per_degree is auto adjust from previous data
-int max_running_time = 3 * 60 *60 ;			 // maximum running time
+byte heater_autoadjust = 1;				   // weather time_per_degree is auto adjust from previous data
+int max_running_time = 3 * 60 *60 ;			 // maximum running time, 3 hours
 
 const int eeprom_pos PROGMEM =100;
-                        // pos 97   time per degree
+                        // pos 96   time per degree (2 bytes)
                         // pos 98    111=saved   0=not saved
                         // pos 99    num_schedules
                         // pos 100   begin saved schedules
@@ -100,12 +101,13 @@ struct dailyprog {
 } ;
 dailyprog schedule[14]; // two schedules per day.
 char week[]={" DLMXJVS"}; // Week begins in 1, sunday
-int num_schedules;
+byte num_schedules;
+byte max_schedules=14;
 
 //Internal variables
-int stop_programm = 0;					    // stop schedule running
-int heater_on=0;						          // heater status
-int sensorValue=0;  // variable to  store  the value  coming  from  the sensor
+byte stop_programm = 0;					    // stop schedule running
+byte heater_on=0;						          // heater status
+// int sensorValue=0;  // variable to  store  the value  coming  from  the sensor
 int count=1;          // Loop count
 float water_temp;			  // current water temperature and target temperature
 float init_temp, final_temp;			    // controls autoadjustment
@@ -114,15 +116,15 @@ int time_to_sched, time_in_sched; //
 time_t date, next_schedule, t_now, boot_time;      // date and time. time left for next use
 time_t init_time, fin_time, running_time;	// running time control
 const uint64_t pipe PROGMEM = 0xE8E8F0F0E1LL;
-int perform_adjust=0;
+byte perform_adjust=0;
 
 LiquidCrystal_I2C lcd(LCD_I2C_ADDR, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 byte char_ON[] = {  B11111,  B10001,  B10101,  B10101,  B10101,  B10101,  B10001,  B11111  };
 byte char_OFF[] = {  B11111,  B10001,  B10001,  B10001,  B10001,  B10001,  B10001,  B11111  };
-const int NUM_DISPLAYS PROGMEM =3; // Num of rotating screens
+const byte NUM_DISPLAYS PROGMEM =3; // Num of rotating screens
 char display_line[4][17]; // LCD lines size
-int display_line_i=1;
-int display_active=1;
+byte display_line_i=1;
+byte display_active=1;
 long int uptime=0;
 int debouncing=300; // min time between button Pressed
 time_t last_interrupt=0;
@@ -134,24 +136,9 @@ tmElements_t timeElements;
 
 void setup() {
   Serial.begin(115200);
-
-  schedule[0].weekday=1;
-  schedule[0].use_time=75;  // 10:15 am.  (10-4)*12 + 15/5=75  75*5=375  375/60 = 6,25 6h 15min + 4.00 = 10,15
-  schedule[1].weekday=1;
-  schedule[1].use_time=38;   // 7:00    7-4=3 3*12=36
-  schedule[2].weekday=2;
-  schedule[2].use_time=39;   // 7:05    7-4=3 3*12=36
-  schedule[3].weekday=3;
-  schedule[3].use_time=40;   // 7:10    7-4=3 3*12=36
-  schedule[4].weekday=4;
-  schedule[4].use_time=41;   // 7:20    7-4=3 3*12=36
-  schedule[5].weekday=5;
-  schedule[5].use_time=42;   // 7:25    7-4=3 3*12=36
-  schedule[6].weekday=6;
-  schedule[6].use_time=43;   // 7:30    7-4=3 3*12=36
-  schedule[7].weekday=7;
-  schedule[7].use_time=198;  // 20:30 am.  (20-4)*12=192 + 30/5=6 198 75*5=375  375/60 = 6,25 6h 15min + 4.00 = 10,15
-  num_schedules=8;
+  // unsave_schedules(); // Set eeprom_pos -2 to 0
+  load_schedules(schedule);
+  show_schedules(schedule);
 
   // Prototype
   pinMode(HeaterPin,OUTPUT);
@@ -168,8 +155,6 @@ void setup() {
   sensorDS18B20.begin();
   // date = get_date(); // Ask for remote date
   // set_date(date);
-  show_schedules(schedule);
-  load_schedules(schedule);
 
   digitalWrite(ProgrammedLed,!stop_programm);
   lcd.begin(16,2,LCD_5x8DOTS);
@@ -198,7 +183,7 @@ void setup() {
     // Fijar a fecha y hora específica. En el ejemplo, 21 de Enero de 2016 a las 03:00:00
     // rtc.adjust(DateTime(2016, 1, 21, 3, 0, 0));
   }
-
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   dt=rtc.now();
 
   char display_line_t [17];
@@ -276,6 +261,76 @@ void loop(){
   count++;
 };
 
+void unsave_schedules(){
+  EEPROM.put(eeprom_pos-2,0);
+}
+
+void save_schedules(){
+  byte mem_count=0;
+  byte next_pos=eeprom_pos;
+  // Serial.println(F("save_schedules"));
+  EEPROM.write(eeprom_pos-2,111);
+  EEPROM.write(eeprom_pos-1,num_schedules);
+  while(mem_count < num_schedules){
+    next_pos=mem_count*sizeof(dailyprog) + eeprom_pos;
+    EEPROM.put(next_pos, schedule[mem_count]);
+    if(debug){
+      Serial.print(F("Sched write: "));
+      Serial.print(schedule[mem_count].weekday);
+      Serial.print(F(" / "));
+      Serial.println(schedule[mem_count].use_time);
+    }
+    mem_count++;
+  }
+  if(debug){Serial.println(F("Saved data to EEPROM"));}
+}
+
+void load_schedules(struct dailyprog *schedule){ // TODO
+  byte mem_count=0;
+  byte next_pos=eeprom_pos;
+  Serial.println(F("load_schedules"));
+  if(EEPROM.read(eeprom_pos-2)==111){                     // Read if EEPROM has saved data. 1 byte.
+    if(debug){Serial.println(F("Loading data from EEPROM"));}
+    time_per_degree = EEPROM.read(eeprom_pos-4);
+    num_schedules=EEPROM.read(eeprom_pos-1);              // Read num_schedules . 1 byte.
+    while(mem_count < num_schedules){
+      next_pos=mem_count*sizeof(dailyprog) + eeprom_pos;
+      schedule[mem_count]=EEPROM.get(next_pos, schedule[mem_count]);
+      if(debug){
+        Serial.print(F("Sched read: "));
+        Serial.print(schedule[mem_count].weekday);
+        Serial.print(F(" / "));
+        Serial.println(schedule[mem_count].use_time);
+      }
+      mem_count++;
+    }
+    // schedule=EEPROM.get(eeprom_pos,dailyprog [14]);
+  }else{
+    schedule[0].weekday=1;
+    schedule[0].use_time=75;  // 10:15 am.  (10-4)*12 + 15/5=75  75*5=375  375/60 = 6,25 6h 15min + 4.00 = 10,15
+    schedule[1].weekday=1;
+    schedule[1].use_time=38;   // 7:00    7-4=3 3*12=36
+    schedule[2].weekday=2;
+    schedule[2].use_time=39;   // 7:05    7-4=3 3*12=36
+    schedule[3].weekday=3;
+    schedule[3].use_time=40;   // 7:10    7-4=3 3*12=36
+    schedule[4].weekday=4;
+    schedule[4].use_time=41;   // 7:20    7-4=3 3*12=36
+    schedule[5].weekday=5;
+    schedule[5].use_time=42;   // 7:25    7-4=3 3*12=36
+    schedule[6].weekday=6;
+    schedule[6].use_time=43;   // 7:30    7-4=3 3*12=36
+    schedule[7].weekday=7;
+    schedule[7].use_time=198;  // 20:30 am.  (20-4)*12=192 + 30/5=6 198 75*5=375  375/60 = 6,25 6h 15min + 4.00 = 10,15
+    num_schedules=8;
+    save_schedules();
+    // EEPROM.put(eeprom_pos,schedule);
+    // EEPROM.write(eeprom_pos-4,time_per_degree);
+    // EEPROM.write(eeprom_pos-2,111);
+    // EEPROM.write(eeprom_pos-1,num_schedules);
+  }
+}
+
 int time_to_minutes(time_t inputtime){
   int return_minutes;
   return_minutes=day(inputtime)*1440 + hour(inputtime)*60 + minute(inputtime);
@@ -318,7 +373,7 @@ void display_show_line(){
 //   delay(50);
 // };
 
-int send_data(){
+void send_data(){
   radio.stopListening();
   Serial.println(F("Now sending"));
   unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
@@ -363,7 +418,7 @@ void stop_programm_int(){
   // };
 };
 
-int set_date(time_t date){  // Need to implement an NTP-like using RF24 get_date()
+void set_date(time_t date){  // Need to implement an NTP-like using RF24 get_date()
   setTime(20,8,00,20,10 ,2018);
   };
 
@@ -371,13 +426,11 @@ time_t get_date(){ // TODO
   // Gets date and time from external source
   };
 
-int load_schedules(struct dailyprog *schedule){}; // TODO
-
-int show_schedules(struct dailyprog *schedule){
+void show_schedules(struct dailyprog *schedule){
     int sch_i;
     int hour,min;
     for(sch_i=0;sch_i<num_schedules;sch_i++){
-      Serial.println(F("Schedule loop"));
+      Serial.print(F("Show schedule: "));
       Serial.print(week[schedule[sch_i].weekday]);
       Serial.print(" - ");
       hour=schedule[sch_i].use_time/12 + 4;
@@ -445,13 +498,13 @@ void print_time(time_t sched){
   Serial.print(second(sched));
 };
 
-int power_on_heater(){ // TODO Implement relay control
+void power_on_heater(){ // TODO Implement relay control
   Serial.println(F("Encendiendo calentador"));
   heater_on=1;
   digitalWrite(HeaterPin, HIGH);
   };
 
-int power_off_heater(){ // TODO Implement relay control
+void power_off_heater(){ // TODO Implement relay control
   Serial.println(F("Apagando calentador"));
   heater_on=0;
   digitalWrite(HeaterPin, LOW);
@@ -459,7 +512,7 @@ int power_off_heater(){ // TODO Implement relay control
 
 int log_session(){}; // TODO
 
-int display(int h_status, float temp1, float temp2, int r_time, time_t today, time_t next){
+void display(byte h_status, float temp1, float temp2, int r_time, time_t today, time_t next){
   char buff1[5], buff2[5];
   dtostrf(temp1,2,2,buff1);
   dtostrf(temp2,2,2,buff2);
@@ -472,6 +525,8 @@ int display(int h_status, float temp1, float temp2, int r_time, time_t today, ti
     lcd.print(display_line[0]);
     lcd.setCursor(0,1);
     lcd.print(display_line[display_line_i]);
+    lcd.setCursor(15,0);
+    lcd.write(h_status);
   }else{ // No display available
     Serial.print(F("Water temperature: "));
     Serial.println(temp1,DEC);
